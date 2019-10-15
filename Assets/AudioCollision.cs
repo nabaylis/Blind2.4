6d07659fc794a4b3faad042332e36955b98d9b98
Neil Patel <neilshiv23@gmail.com>
2019-10-15 00:56:46 -0400

    AudioCollision Script

---
+++ Assets/AudioCollision.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioCollision : MonoBehaviour
+{
+    public AudioClip impact;
+    AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+
+    void OnTriggerEnter(Collider food)
+    {
+        if (food.gameObject.tag == "Food")
+        {
+            audioSource.PlayOneShot(impact, 0.7f);
+        }
+    }
+}
